c8c408c6d18994094241f05e5dec3e7b379cbbf9
MichalPaczek <m.paczek07@gmail.com>
2025-04-10 08:47:52 +0200

    comit_10.04.2025

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,29 +1,23 @@
 from django.http import HttpResponse
-from .models import Question
+from .models import Choice, Question
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.urls import reverse
+from django.db.models import F
+from django.http import HttpResponse,HttpResponseRedirect
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
